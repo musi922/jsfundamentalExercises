a13d830b6e392eb40479f5ee64aaaf5a3f1b0160
Richard <richardmusime6@gmail.com>
2024-05-28 08:28:34 +0200

    adding third bundle second solution

--- thirdbundle.js
+++ thirdbundle.js
@@ -10,3 +10,9 @@ let numbers = (num)=>{
 }
 console.log(numbers([1,2,3,4]));
 
+//findind number of evenn digits in an arrray
+
+let evenn = (arr)=> arr.filter(el=> el%2===0).length;
+console.log(evenn([1,2,3,4]));
+console.log(evenn([1,2,3,4,5,6,7,8]));
+console.log(evenn([1,2,3,4,9,10,12,11]));
