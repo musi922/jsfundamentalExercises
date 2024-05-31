2089f31ddd1840a70d76f207598b2474d3e106fc
Richard <richardmusime6@gmail.com>
2024-05-31 08:38:18 +0200

    Adding the second exercise where given a string and count all vowels in it and then count all vowels in them

--- fifthbundle.js
+++ fifthbundle.js
@@ -1,5 +1,15 @@
 //given like string: 'webmaster'
 //you return like string : abeemrstw
 
-const givenstring = (str)=>str.split('').sort().join('')
+const givenstring = (str)=>str.split('').sort((a,b)=>a.charCodeAt(0)-b.charCodeAt(0)).join('')
 console.log(givenstring('webmaster'))
+
+//second exercise
+//string and counyt number of vowels
+
+const giventext = (stri)=>{
+    let result = stri.match(/[a,e,i,o,u,A,E,I,O,U]/g).length;
+    return result
+}
+console.log(giventext('webmaster'))
+console.log(giventext('RichardmusimeusbhgdusnmxA'))
